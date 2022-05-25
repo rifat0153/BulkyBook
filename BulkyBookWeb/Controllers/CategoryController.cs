3e811b4872a0549971661b53fc2c9d1af3958b7e
Mahbubur Rahman <rifat0153@gmail.com>
2022-05-26 02:22:54 +0600

    Category EDIT view added

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -20,7 +20,7 @@ namespace BulkyBookWeb.Controllers
             return View(objCategoryList);
         }
 
-        
+
         public IActionResult Create()
         {
             return View();
@@ -30,12 +30,51 @@ namespace BulkyBookWeb.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Category obj)
         {
-            if(obj.Name == obj.DisplayOrder.ToString())
+            if (obj.Name == obj.DisplayOrder.ToString())
             {
                 ModelState.AddModelError("Name", "The DisplayOrder cannot match the Name");
             }
 
-            if (ModelState.IsValid) { 
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(obj);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var categoryFromDB = _db.Categories.FirstOrDefault(c => c.Id == id);
+
+            if (categoryFromDB == null)
+            {
+                return NotFound();
+            }
+
+            Console.WriteLine(categoryFromDB.ToString());
+
+            return View(categoryFromDB);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "The DisplayOrder cannot match the Name");
+            }
+
+            if (ModelState.IsValid)
+            {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
                 return RedirectToAction("Index");
